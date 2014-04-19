8e44cd7744eadb5d42a41b1dbc15a4f4e505d477
OkayHughes <OkayHughes@gmail.com>
2014-04-19 14:22:57 -0400

    Add signin functionality, i.e user and session controllers, views, and models. Included bcrypt gem.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Example::Application.routes.draw do
   resources :posts
 
   root "posts#index"
+  #user resources 
+  resources :users
+  #sessions methods
+  resources :sessions, only: [:new, :create, :destroy]
+  get "/sessions" => "sessions#index"
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419160017) do
+ActiveRecord::Schema.define(version: 20140419181040) do
 
   create_table "posts", force: true do |t|
     t.text     "title"
@@ -19,5 +19,18 @@ ActiveRecord::Schema.define(version: 20140419160017) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.string   "group"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
