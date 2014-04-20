9f0d722528fb7ed920daafa34b31bfe9b80b6ad3
Caleb Jones <bionicle.dragon@gmail.com>
2014-04-20 17:15:10 +0000

    Added editing posts Some style changes Distaster control on my code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,21 +4,46 @@ def index
 	@posts = Post.all
 end
 
-def new
-	@post = Post.new
-end
-
 def show
 	@post = Post.find(params[:id])
+end
+
+def new
+	@post = Post.new
 end
 
 def create
 	@post = Post.new(post_params)
 	@post.user = current_user
 
-	@post.save
-	redirect_to @post
+	if @post.save
+		redirect_to @post
+	else
+		render 'new'
+	end
 end
+
+def edit
+	@post = Post.find(params[:id])
+end
+
+def update
+	@post = Post.find(params[:id])
+
+	if @post.update(post_params)
+		redirect_to @post
+	else
+		render 'edit'
+	end
+end
+
+def destroy
+	@post = Post.find(params[:id])
+	@post.destroy
+
+	redirect_to posts_path
+end
+
 
 private
 	def post_params
