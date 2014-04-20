7c6d588d127bd9a4c37d372becd01b3e21c0d69d
OkayHughes <OkayHughes@gmail.com>
2014-04-20 14:43:32 -0400

    Fixed regex for email, too restrictive before. Fixed bug about complaining when there are numbers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	has_many :posts
 	validates :name, presence: true, length: {maximum: 50}
 	validates :group, presence: true
-	VALID_EMAIL_REGEX = /A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
 	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
 		uniqueness: {case_sensitive: false}
 	has_secure_password
